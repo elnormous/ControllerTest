27a8979af360e0689015466bdae993c519853c37
Elviss Strazdins <elviss@elviss.lv>
2014-01-12 00:27:22 +0200

    First commit

---
+++ ControllerTest/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  ControllerTest
+//
+//  Created by Elviss Strazdins on 10.01.2014.
+//  Copyright (c) 2014 Elviss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <GameController/GameController.h>
+
+@interface ViewController : UIViewController
+
+@property IBOutlet UITextView* textView;
+@property IBOutlet UITextField* dPadView;
+@property IBOutlet UITextField* aButtonView;
+@end
---
+++ ControllerTest/ViewController.m
@@ -0,0 +1,103 @@
+//
+//  ViewController.m
+//  ControllerTest
+//
+//  Created by Elviss Strazdins on 10.01.2014.
+//  Copyright (c) 2014 Elviss. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+-(void)log: (NSString*)text
+{
+	_textView.text = [NSString stringWithFormat:@"%@%@\n", _textView.text, text];
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	
+	[self log: @"Started"];
+	
+	[self initGameController];
+}
+
+-(void) handleConnected:(NSNotification *)note
+{
+	[self log: @"Connected"];
+	
+	[self setUpDevices];
+}
+
+-(void) setUpDevices
+{
+	NSArray* controllers = GCController.controllers;
+	
+	//search for the first attached controller
+	for (GCController* controller in controllers)
+	{
+		[self log: [NSString stringWithFormat: @"%@ (%d)", controller.vendorName, controller.attachedToDevice]];
+		
+		if (controller.attachedToDevice)
+		{
+			controller.gamepad.dpad.valueChangedHandler = ^(GCControllerDirectionPad* dpad, float xValue, float yValue) {
+				
+				_dPadView.text = [NSString stringWithFormat: @"D pad: %f, %f", xValue, yValue];
+			};
+			
+			controller.gamepad.buttonA.valueChangedHandler = ^(GCControllerButtonInput* button, float value, BOOL pressed) {
+				
+				_aButtonView.text = [NSString stringWithFormat: @"A: %f, %d", value, pressed];
+			};
+			
+			if (controller.extendedGamepad)
+			{
+				controller.extendedGamepad.leftThumbstick.valueChangedHandler = ^(GCControllerDirectionPad* dpad, float xValue, float yValue) {
+					_dPadView.text = [NSString stringWithFormat: @"Left thumbstick: %f, %f", xValue, yValue];
+				};
+			}
+			
+			
+			break;
+		}
+	}
+}
+
+-(void) handleDisconnected:(NSNotification *)note
+{
+	[self log: @"Disconnected"];
+}
+
+-(void) initGameController
+{
+	[self log: @"Searching for devices"];
+	
+	[[NSNotificationCenter defaultCenter] addObserver:self
+											 selector:@selector(handleConnected:)
+												 name:GCControllerDidConnectNotification
+											   object:nil];
+	
+	[[NSNotificationCenter defaultCenter] addObserver:self
+											 selector:@selector(handleDisconnected:)
+												 name:GCControllerDidDisconnectNotification
+											   object:nil];
+	
+	[GCController startWirelessControllerDiscoveryWithCompletionHandler:
+	 ^(void){ [self log:@"Disconvery finished"]; }];
+	
+	[self setUpDevices];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
